9101405e4338fc6a6061490fa26f64f70d97e26a
benlay <v.kaltschmidt@gmx.de>
2022-04-29 15:05:36 +0200

    Delete marbles directory

--- marbles/moreblocks.lua
+++
@@ -1,8 +0,0 @@
-stairsplus:register_all("moreblocks", "marbles:marbles", "marbles:marbles", {
-    description = "Marbles",
-	tiles = {"marbles.png"},
-	is_ground_content = true,
-	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults(),
-})
-
