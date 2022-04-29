56cbad34c998d3a82cf78909b7f54899e7ff2abd
benlay <v.kaltschmidt@gmx.de>
2022-04-29 15:06:09 +0200

    Add files via upload

---
+++ moreblocks.lua
@@ -0,0 +1,8 @@
+stairsplus:register_all("moreblocks", "marbles:marbles", "marbles:marbles", {
+    description = "Marbles",
+	tiles = {"marbles.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
